3706b7088fc0e5afe99c9d01505508f5e5b30fd8
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2016-12-29 19:39:46 +0200

    Runner (#5)
    * Default learning_rate is 10
    
    * process input folder
    
    * some fixes
    
    * another fixes
    
    * fixes
    
    * utf-8 fix
    
    * small log fix
    
    * fixed expanduser for output_path
    
    * optimizer added to the output file name
    
    * added content and style layers to path, folders creation
    
    * ops
    
    * iter count added to file name
    
    * fixed live output
    
    * fix styling
    
    * fix syntax
    
    * fixing some linter warnings
    
    * update in readme.md

--- run_neural_style.py
+++ run_neural_style.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
+from __future__ import print_function
+import sys
 import subprocess
 from datetime import datetime
 from argparse import ArgumentParser
-from os.path import basename, splitext, expanduser
+from os.path import basename, splitext, expanduser, isfile, join, exists
+from os import listdir, getcwd, makedirs
 
 RUN_SCRIPT_NAME = "neural_style.lua"
+DEF_CONTENT_LAYERS = 'relu4_2'
+DEF_STYLE_LAYERS = 'relu1_1,relu2_1,relu3_1,relu4_1,relu5_1'
+
 
 def build_parser():
     parser = ArgumentParser()
+
     # Basic options
     parser.add_argument('-style_image', type=str,
                         default='examples/inputs/seated-nude.jpg',
@@ -23,7 +29,8 @@ def build_parser():
     '''
     parser.add_argument('-content_image', type=str,
                         default='examples/inputs/tubingen.jpg',
-                        dest='content_image', help='Content target image',
+                        dest='content_image',
+                        help='Content target image or folder with images',
                         metavar='CONTENT_IMAGE', required=True)
 
     parser.add_argument('-image_size', type=int,
@@ -59,7 +66,9 @@ def build_parser():
                         choices=['random', 'image'])
     parser.add_argument('-optimizer', default='lbfgs', dest='optimizer',
                         choices=['lbfgs', 'adam'])
-    parser.add_argument('-learning_rate', default=5, type=float,
+    parser.add_argument('-lbfgs_num_correction', default=0, dest='lbfgs_num_correction',
+                        type=int)
+    parser.add_argument('-learning_rate', default=10, type=float,
                         dest='learning_rate')
 
     # Output options
@@ -69,6 +78,16 @@ def build_parser():
                         dest='save_iter')
     parser.add_argument('-output_path', default=None, type=str,
                         dest='output_path')
+    parser.add_argument('-input_file_as_folder', dest='input_file_as_folder',
+                        action='store_true')
+    parser.add_argument('-no-input_file_as_folder', dest='input_file_as_folder',
+                        action='store_false')
+    parser.set_defaults(input_file_as_folder=True)
+    parser.add_argument('-style_as_folder', dest='style_as_folder',
+                        action='store_true')
+    parser.add_argument('-no-style_as_folder', dest='style_as_folder',
+                        action='store_false')
+    parser.set_defaults(style_as_folder=True)
 
     # Other options
     parser.add_argument('-style_scale', default=1.0, type=float,
@@ -93,11 +112,11 @@ def build_parser():
     # Layers options
     parser.add_argument('-content_layers', type=str,
                         help='layers for content',
-                        default='relu4_2',
+                        default=DEF_CONTENT_LAYERS,
                         dest='content_layers')
     parser.add_argument('-style_layers', type=str,
                         help='layers for style',
-                        default='relu1_1,relu2_1,relu3_1,relu4_1,relu5_1',
+                        default=DEF_STYLE_LAYERS,
                         dest='style_layers')
 
     # Runner options
@@ -105,41 +124,55 @@ def build_parser():
                         default=False,
                         dest='time_markers')
 
-    # TODO: style_layer_weights
+    # TODO: style_blend_weights, style_layer_weights
 
     return parser
 
 
-# def check_opts(opts):
-
-
-def main():
-    parser = build_parser()
-    opts = parser.parse_args()
-    # check_opts(opts)
-
-    output_dir = opts.output_path if opts.output_path != None else os.getcwd()
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    out_file_name = '{style_image}_{content_image}_cw{content_weight}_sw{style_weight}_\
-lr{learning_rate}_sc{style_scale}_tv{tv_weight}'.format(
-    style_image=splitext(basename(opts.style_image))[0],
-    content_image=splitext(basename(opts.content_image))[0],
-    content_weight='%g'%(opts.content_weight),
-    style_weight='%g'%(opts.style_weight),
-    learning_rate='%g'%(opts.learning_rate),
-    style_scale='%g'%(opts.style_scale),
+def run_on_file(opts, input_file):
+    output_dir = expanduser(opts.output_path) if opts.output_path is not None else getcwd()
+    if opts.style_as_folder is True:
+        output_dir = join(output_dir, splitext(basename(opts.style_image))[0])
+    if opts.input_file_as_folder is True:
+        output_dir = join(output_dir, splitext(basename(input_file))[0])
+    if not exists(output_dir):
+        makedirs(output_dir)
+    optimizer_str = opts.optimizer
+    if opts.optimizer == 'adam':
+        optimizer_str += '_lr{0}'.format('%g' % (opts.learning_rate))
+    if opts.optimizer == 'lbfgs' and opts.lbfgs_num_correction > 0:
+        optimizer_str += '_numcorr{0}'.format('%g' % (opts.lbfgs_num_correction))
+    out_file_name = '{style_image}{content_image}{sep1}i{iter}cw{content_weight}_sw{style_weight}_\
+{optimizer}_sc{style_scale}_tv{tv_weight}'.format(
+    style_image=splitext(basename(opts.style_image))[0] if opts.style_as_folder is False else '',
+    content_image=('_'+splitext(basename(input_file))[0]) if opts.input_file_as_folder is False else '',
+    sep1='_' if opts.input_file_as_folder is False and opts.style_as_folder is False else '',
+    iter=opts.num_iter,
+    content_weight='%g' % (opts.content_weight),
+    style_weight='%g' % (opts.style_weight),
+    optimizer=optimizer_str,
+    style_scale='%g' % (opts.style_scale),
     tv_weight=opts.tv_weight
     )
     if opts.normalize_gradients:
         out_file_name += '_norm'
     if opts.original_colors:
         out_file_name += '_colors'
+    if opts.style_layers != DEF_STYLE_LAYERS:
+        # relu1_1,relu2_1,relu3_1,relu4_1,relu5_1
+        style_layers = opts.style_layers.replace('relu', '') \
+                                        .replace('_', '').replace(',', '-')
+        out_file_name += '_sl{0}'.format(style_layers)
+    if opts.content_layers != DEF_CONTENT_LAYERS:
+        # relu1_1,relu2_1,relu3_1,relu4_1,relu5_1
+        content_layers = opts.content_layers.replace('relu', '') \
+                                        .replace('_', '').replace(',', '-')
+        out_file_name += '_cl{0}'.format(content_layers)
     if opts.time_markers:
         now = datetime.now()
         out_file_name += '_{0}'.format(now.strftime('%Y%m%d_%H%M%S'))
     out_file_name += '.jpg'
-    out_file_path = os.path.join(output_dir, out_file_name)
+    out_file_path = join(output_dir, out_file_name)
 
     run_script = 'th {script} -style_scale {style_scale} \
 -init {init} -style_image "{style_image}" \
@@ -155,12 +188,13 @@ lr{learning_rate}_sc{style_scale}_tv{tv_weight}'.format(
 -seed {seed} {normalize_gradients} \
 -learning_rate {learning_rate} \
 -original_colors {original_colors} {cudnn_autotune} \
+-lbfgs_num_correction {lbfgs_num_correction} \
 -pooling {pooling} -proto_file {proto_file} -model_file {model_file}'.format(
     script=RUN_SCRIPT_NAME,
     style_scale=opts.style_scale,
     init=opts.init,
     style_image=expanduser(opts.style_image),
-    content_image=expanduser(opts.content_image),
+    content_image=input_file,
     image_size=opts.image_size,
     output_image=out_file_path,
     content_weight=opts.content_weight,
@@ -178,20 +212,64 @@ lr{learning_rate}_sc{style_scale}_tv{tv_weight}'.format(
     normalize_gradients=('-normalize_gradients' if opts.normalize_gradients else ''),
     learning_rate=opts.learning_rate,
     original_colors=('1' if opts.original_colors else '0'),
+    lbfgs_num_correction=opts.lbfgs_num_correction,
     cudnn_autotune=('-cudnn_autotune' if opts.cudnn_autotune else ''),
     pooling=opts.pooling,
     proto_file=opts.proto_file,
     model_file=opts.model_file)
 
     print('Script \'{0}\''.format(run_script))
-
     with subprocess.Popen(run_script, shell=True,
                           stdin=subprocess.PIPE,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
                           universal_newlines=True) as proc:
-        print(proc.stdout.read())
-        print(proc.stderr.read())
+        for line in iter(proc.stdout.readline, ''):
+            sys.stdout.write(line)
+
+
+# Print iterations progress
+def print_progress(iteration, total, prefix='', suffix='', decimals=1,
+                   barLength=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    formatStr = "{0:." + str(decimals) + "f}"
+    percent = formatStr.format(100 * (iteration / float(total)))
+    filledLength = int(round(barLength * iteration / float(total)))
+    bar_str = '█' * filledLength + '-' * (barLength - filledLength)
+    text = '\r%s |%s| %s%s %s' % (prefix, bar_str, percent, '%', suffix)
+    sys.stdout.write(text),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+def main():
+    parser = build_parser()
+    opts = parser.parse_args()
+
+    input_path = expanduser(opts.content_image)
+    print('Input path is {0}'.format(input_path))
+    if isfile(input_path):
+        run_on_file(opts, input_path)
+    else:
+        input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+        count = len(input_files)
+        i = 1
+        for input_filename in input_files:
+            print('\nInput file is {0}'.format(input_filename))
+            run_on_file(opts, join(input_path, input_filename))
+            print_progress(i, count, prefix='Progress:',
+                           suffix='Complete', barLength=100)
+            i += 1
 
 if __name__ == '__main__':
     main()
